5206b7559aa92e0b788c5b10e7124471ba462273
rabiasohail-rs <165444954+rabiasohail-rs@users.noreply.github.com>
2024-04-26 10:00:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1 to 10",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
